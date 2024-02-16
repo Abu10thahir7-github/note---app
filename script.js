c61adf2f9a95861df7bb176d928fd85276307377
Abu10thahir7-github <142816135+Abu10thahir7-github@users.noreply.github.com>
2024-02-16 14:46:20 +0530

    \\

--- script.js
+++ script.js
@@ -4,9 +4,36 @@
 
 
 
+
+
+
 function submit(){
-  var  title =  document.getElementById('title').value;
-  var  text = document.getElementById('text-Box').value;
     
+    var title =  document.getElementById('title').value;
+    var text = document.getElementById('inputBox').value;
   
+    localStorage.setItem('title',title);
+    localStorage.setItem('text',text);
+   
+    if(title == "" || text == ""){
+        alert("Please enter the title and text");
+    }
+    else{
+
+        var add = document.getElementById('add-Items');
+        var option = document.createElement('option');
+        option.text = title;
+        add.add(option, add[0]);
+
+    
+
+    }
+
+   
+
 }
+
+
+
+
+
